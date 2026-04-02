231b4ff0c1a2b9c8d306bfd9c77659dbe1e6955d
Salauddin Ansari <salauddinansari07457@gmail.com>
2026-04-02 13:42:24 +0530

    Main SpringBoot .java file

---
+++ src/main/java/com/contentpulse/ContentPulseApplication.java
@@ -0,0 +1,13 @@
+package com.contentpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class ContentPulseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ContentPulseApplication.class, args);
+    }
+}
